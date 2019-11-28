1cff3cf4869d3323a0d793a90bf8cded261e2e90
abraler <571432031@qq.com>
2019-11-28 23:36:19 +0800

    1

---
+++ src/Main2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int line = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i < line; i++) {
+            System.out.println(scanner.nextLine().replaceAll("(.)\\1+","$1$1").replaceAll("(.)\\1(.)\\2","$1$1$2"));
+        }
+    }
+}
